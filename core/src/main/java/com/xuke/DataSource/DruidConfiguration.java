2d4f1b8e89e3f686f9f557bf66cbbfc899c4bcfb
XuKer <wxk1035@gmail.com>
2020-02-16 23:23:02 +0800

    添加一些说明

--- core/src/main/java/com/xuke/DataSource/DruidConfiguration.java
+++ core/src/main/java/com/xuke/DataSource/DruidConfiguration.java
@@ -41,7 +41,7 @@ public class DruidConfiguration {
         //白名单：
         servletRegistrationBean.addInitParameter("allow","127.0.0.1");
         //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
-        servletRegistrationBean.addInitParameter("deny","192.168.1.73");
+        servletRegistrationBean.addInitParameter("deny","192.168.109.126");
         //登录查看信息的账号密码.
         servletRegistrationBean.addInitParameter("loginUsername","admin");
         servletRegistrationBean.addInitParameter("loginPassword","123456");
